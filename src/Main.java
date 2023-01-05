3387e4eeb17e41b8ddb10c31f6e4275bd3ef0bdc
Bekzat22 <121627906+Bekzat22@users.noreply.github.com>
2023-01-05 18:42:07 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -14,17 +14,14 @@ public class Main {
      public static String Ddd(int age, int temperature) {
 
 
-         if(age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30){
-             System.out.println("Можно идти гулять");
-         } else if(age < 20 && temperature >= 0 && temperature <= 28){
-             System.out.println("Можно идти гулять");
-         } else if(age > 45 && temperature >= -10 && temperature <= 25){
-             System.out.println("Можно идти гулять");
-         } else{
-             System.out.println("Оставайтесь дома");
-         }
+        if(age >= 20 && age <= 45 && temperature >= -20 && temperature <= 30)
+            return "Можно идти гулять";
+        else if(age < 20 && temperature >= 0 && temperature <= 28)
+            return "Можно идти гулять";
+        else if(age > 45 && temperature >= -10 && temperature <= 25)
+            return "Можно идти гулять";
 
-         return "Age = " + age   + " "+"Temperature =" + temperature;
+        return "Отсавайтесь дома";
 
      }
 }
